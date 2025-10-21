52567201d22bba08d76a80f3bb46485224564674
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:57:13 -0300

    feat: adiciona tratamento de exceções e mensagens de log nos métodos de FornecedorDAO

--- src/main/java/DAO/FornecedorDAO.java
+++ src/main/java/DAO/FornecedorDAO.java
@@ -18,17 +18,21 @@ public class FornecedorDAO {
                 INSERT INTO Fornecedor (nome, cnpj)
                 VALUES (?,?)
                 """;
-        try(Connection conn = Conexao.conectar();
-            PreparedStatement ps = conn.prepareStatement(sql)){
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setString(1, fornecedor.getNome());
             ps.setString(2, fornecedor.getCnpj());
             ps.executeUpdate();
+            System.out.println("[OK] Fornecedor cadastrado com sucesso!");
+        } catch (SQLException e){
+            System.err.println("[ERRO] Erro ao cadastrar fornecedor: " + e.getMessage());
+            e.printStackTrace();
         }
 
     }
 
-    public boolean existeCnpj (String cnpj) throws SQLException{
+    public boolean existeCnpj(String cnpj) throws SQLException {
         boolean existe = false;
 
         String sql = """
@@ -41,15 +45,19 @@ public class FornecedorDAO {
 
             ps.setString(1, cnpj);
             var rs = ps.executeQuery();
-            if (rs.next()){
+            if (rs.next()) {
                 existe = true;
+                System.out.println("[AVISO] CNPJ já cadastrado no sistema.");
             }
+        } catch (SQLException e){
+            System.err.println("[ERRO] Erro ao verificar existência do CNPJ: " + e.getMessage());
+            e.printStackTrace();
         }
         return existe;
     }
 
-    public List<Fornecedor> listarFornecedores()throws SQLException {
-        List<Fornecedor>fornecedores = new ArrayList<>();
+    public List<Fornecedor> listarFornecedores() throws SQLException {
+        List<Fornecedor> fornecedores = new ArrayList<>();
 
         String sql = """
                 SELECT id, nome, cnpj
@@ -57,11 +65,11 @@ public class FornecedorDAO {
                 """;
 
         try (Connection conn = Conexao.conectar();
-             PreparedStatement ps = conn.prepareStatement(sql)){
+             PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ResultSet rs = ps.executeQuery();
 
-            while (rs.next()){
+            while (rs.next()) {
                 Fornecedor fornecedor = new Fornecedor();
                 fornecedor.setId(rs.getInt("id"));
                 fornecedor.setNome(rs.getString("nome"));
@@ -69,6 +77,9 @@ public class FornecedorDAO {
 
                 fornecedores.add(fornecedor);
             }
+        } catch (SQLException e) {
+            System.err.println("[ERRO] Erro ao listar fornecedores: " + e.getMessage());
+            e.printStackTrace();
         }
         return fornecedores;
     }
