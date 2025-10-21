de03d68de465bbbbc7592204fe87af08d1507127
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:12:13 -0300

    feat: adiciona metodos construtores e getters e setters na classe NotaEntrada

--- src/main/java/Model/NotaEntrada.java
+++ src/main/java/Model/NotaEntrada.java
@@ -7,4 +7,40 @@ public class NotaEntrada {
     private int id;
     private Fornecedor idFornecedor;
     private Date dataEntrada;
+
+    public NotaEntrada(){}
+
+    public NotaEntrada(int id, Fornecedor idFornecedor, Date dataEntrada) {
+        this.id = id;
+        this.idFornecedor = idFornecedor;
+        this.dataEntrada = dataEntrada;
+    }
+    public NotaEntrada(Fornecedor idFornecedor, Date dataEntrada) {
+        this.idFornecedor = idFornecedor;
+        this.dataEntrada = dataEntrada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Fornecedor getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(Fornecedor idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
 }
