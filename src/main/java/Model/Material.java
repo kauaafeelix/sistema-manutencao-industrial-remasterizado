6aa3562c5028771fe5ad1683055a97006cab63c6
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:12:05 -0300

    feat: adiciona metodos construtores e getters e setters na classe Material

--- src/main/java/Model/Material.java
+++ src/main/java/Model/Material.java
@@ -6,4 +6,50 @@ public class Material {
     private String unidade;
     private double estoque;
 
+    public Material(){}
+
+    public Material(int id, String nome, String unidade, double estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.unidade = unidade;
+        this.estoque = estoque;
+    }
+
+    public Material(String nome, String unidade, double estoque) {
+        this.nome = nome;
+        this.unidade = unidade;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public double getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(double estoque) {
+        this.estoque = estoque;
+    }
 }
