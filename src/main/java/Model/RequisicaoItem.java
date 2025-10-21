dbccc800714ea8ad2fedede79a61812f00c38436
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:12:32 -0300

    feat: adiciona metodos construtores e getters e setters na classe RequisicaoItem

--- src/main/java/Model/RequisicaoItem.java
+++ src/main/java/Model/RequisicaoItem.java
@@ -4,4 +4,37 @@ public class RequisicaoItem {
     private int idRequisicao;
     private Material idMaterial;
     private double quantidade;
+
+    public RequisicaoItem() {
+    }
+
+    public RequisicaoItem(int idRequisicao, Material idMaterial, double quantidade) {
+        this.idRequisicao = idRequisicao;
+        this.idMaterial = idMaterial;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdRequisicao() {
+        return idRequisicao;
+    }
+
+    public void setIdRequisicao(int idRequisicao) {
+        this.idRequisicao = idRequisicao;
+    }
+
+    public Material getIdMaterial() {
+        return idMaterial;
+    }
+
+    public void setIdMaterial(Material idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
 }
