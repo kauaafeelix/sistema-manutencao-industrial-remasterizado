1bd7d0b7b192d442c9ca6f8cd9ef51ca2861e7ca
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 14:50:46 -0300

    feat: adiciona Conexao entre java e banco de dados

--- src/main/java/Conexao/Conexao.java
+++ src/main/java/Conexao/Conexao.java
@@ -1,5 +1,15 @@
 package Conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
 
+        private static final String URL = "jdbc:mysql://localhost:3306/manutencao_remasterizada?useSSL=false&serverTimezone=UTC";
+        private static final String USUARIO = "root";
+        private static final String SENHA = "mysqlPW";
+
+        public static Connection conectar() throws SQLException {
+            return DriverManager.getConnection(URL,USUARIO,SENHA);
+        }
 }
