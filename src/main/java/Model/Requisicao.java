f4d8d0eb8ecd6c4a5630bce08d1366b0ce41fc12
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:12:24 -0300

    feat: adiciona metodos construtores e getters e setters na classe Requisicao

--- src/main/java/Model/Requisicao.java
+++ src/main/java/Model/Requisicao.java
@@ -7,4 +7,40 @@ public class Requisicao {
     private int id;
     private String setor;
     private Date dataSolicitacao;
+
+    public Requisicao(){}
+
+    public Requisicao(int id, String setor, Date dataSolicitacao) {
+        this.id = id;
+        this.setor = setor;
+        this.dataSolicitacao = dataSolicitacao;
+    }
+    public Requisicao(String setor, Date dataSolicitacao) {
+        this.setor = setor;
+        this.dataSolicitacao = dataSolicitacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public Date getDataSolicitacao() {
+        return dataSolicitacao;
+    }
+
+    public void setDataSolicitacao(Date dataSolicitacao) {
+        this.dataSolicitacao = dataSolicitacao;
+    }
 }
