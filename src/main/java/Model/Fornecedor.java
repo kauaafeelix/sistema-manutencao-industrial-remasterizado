d3ca30550c2fb3e2c6ab4dd79df8d47b281b2920
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-21 15:11:58 -0300

    feat: adiciona metodos construtores e getters e setters na classe Fornecedor

--- src/main/java/Model/Fornecedor.java
+++ src/main/java/Model/Fornecedor.java
@@ -6,6 +6,40 @@ public class Fornecedor {
     private String cnpj;
 
 
+    public Fornecedor(){}
 
+    public Fornecedor(int id, String nome, String cnpj) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
 
+    public Fornecedor(String nome, String cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
